f7190b223a1d5d775a8a966c7c09da586714bca8
filipefilardi <filipefilardi@hotmail.com>
2015-03-20 09:43:54 -0300

    Create StringUtils.java

---
+++ StringUtils.java
@@ -0,0 +1,15 @@
+public class StringUtils {
+
+	public static String fillOrTruncate(String str, int size, char c) {
+		if (str.length() < size) {
+			StringBuffer buff = new StringBuffer(str);
+			while (buff.length() < size) {
+				buff.append(c);
+			}
+
+			return buff.toString();
+		} else {
+			return str.substring(0, size);
+		}
+	}
+}
