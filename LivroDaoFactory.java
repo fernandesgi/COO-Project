4d844b18f4395677e47417a5cfac7faee490baf5
filipefilardi <filipefilardi@hotmail.com>
2015-04-24 15:59:58 -0300

    Delete LivroDaoFactory.java

--- LivroDaoFactory.java
+++
@@ -1,9 +0,0 @@
-public class LivroDaoFactory {
-
-	// Devolve um LivroDao (interface)
-	public LivroDao getInstance() {
-		return new LivroDaoByObjectOutputStream();
-		//return new LivroDaoByRandomAccessFile();
-		//return new LivroDaoBySequencialAccessByteFile();
-	}
-}
